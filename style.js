8d22564bd54862eafb35e711b2736e1daac84e19
Haller2000 <hallerpeter7@gmail.com>
2024-10-24 13:41:11 +0200

    js

--- style.js
+++ style.js
@@ -6,3 +6,39 @@ function changeImage() {
         image.src = "kepek/image1.png";
     }
 }
+
+function changeImage2() {
+    var image = document.getElementById('myImage2');
+    if (image.src.match("2")) {
+        image.src = "kepek/image2.jfif";
+    } else {
+        image.src = "kepek/2.jpg";
+    }
+}
+
+function changeImage3() {
+    var image = document.getElementById('myImage3');
+    if (image.src.match("3")) {
+        image.src = "kepek/image2.jfif";
+    } else {
+        image.src = "kepek/3.jpg";
+    }
+}
+
+function changeImage4() {
+    var image = document.getElementById('myImage4');
+    if (image.src.match("")) {
+        image.src = "kepek/image2.jfif";
+    } else {
+        image.src = "kepek/image1.png";
+    }
+}
+
+function changeImage5() {
+    var image = document.getElementById('myImage5');
+    if (image.src.match("image1")) {
+        image.src = "kepek/image2.jfif";
+    } else {
+        image.src = "kepek/image1.png";
+    }
+}
